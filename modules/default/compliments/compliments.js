a84572efdea7760b8c24ca722b261c4a6b481107
Matthew Chadwick <matthew.chadwick@gmail.com>
2018-01-02 15:03:29 -0600

    some new compliments

--- modules/default/compliments/compliments.js
+++ modules/default/compliments/compliments.js
@@ -12,22 +12,55 @@ Module.register("compliments", {
 	defaults: {
 		compliments: {
 			anytime: [
-				"Hey there sexy!"
+				//"Hey there sexy!",
+				"A little love, a little trust, a lot of forgiveness",
+				"Be the change you wish to see in the world.",
+				"Five hundred twenty-five thousand six hundred minutes",
+				"Give me your own special smile",
+				"Have you ever tried to eat a clock?",
+ 				"I am a unique child of this world.",
+				"I am blessed with an incredible family and wonderful friends.",
+				"I am courageous and I stand up for myself.",
+ 				"I draw from my inner strength and light.",
+ 				"I embrace the rhythm and the flowing of my own heart.",
+				"I kissed your honey hair with my grateful tears.",
+				"I love and approve of myself.",
+ 				"I love deadlines. I like the whooshing sound they make as they fly by.",
+ 				"I muster up more hope and courage from deep inside me.",
+ 				"I replace my anger with understanding and compassion.",
+				"If I can't dance, I don't want to be part of your revolution",
+				"It's all happening",
+ 				"It's up to you. You have to make the big decisions.",
+				"It's very time consuming.",
+				"I've gone to look for America",
+				"I've had a lot of worries in my life, most of which never happened.",
+				"My thoughts are filled with positivity and my life is plentiful with prosperity.",
+ 				"People who think they know everything are a great annoyance to those of use who do.",
+				"Remember the heart is a tender thing\nRestless and hopeful It cries and sings",
+				"The bright blessed day, the dark sacred night",
+				"Time passes much too quickly\nWhen we're together laughing",
+ 				"You can't always get want you want",
+				"You just call out my name\nand you know wherever I am \nI'll come runnin'\nto see you again",
+				"You're talkin' a lot, but you're not sayin' anything.",
+ 				"Wonderful things unfold before me.",
+				"Who am I, to be blind? Pretending not to see their needs",
 			],
 			morning: [
-				"Good morning, handsome!",
-				"Enjoy your day!",
-				"How was your sleep?"
+				//"Good morning, handsome!",
+				//"Enjoy your day!",
+				//"How was your sleep?"
+				"I wake up today with strength in my heart and clarity in my mind.",
+				"Today will be a georgeous day to remember.",
 			],
 			afternoon: [
-				"Hello, beauty!",
-				"You look sexy!",
-				"Looking good today!"
+				//"Hello, beauty!",
+				//"You look sexy!",
+				//"Looking good today!"
 			],
 			evening: [
-				"Wow, you look hot!",
-				"You look nice!",
-				"Hi, sexy!"
+				//"Wow, you look hot!",
+				//"You look nice!",
+				//"Hi, sexy!"
 			]
 		},
 		updateInterval: 30000,
